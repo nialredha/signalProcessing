9d77995fb1213562e2a1371596f4f760aa56ab0f
nred <nialredha@gmail.com>
2021-06-20 20:56:41 -0500

    got addition working. thanks to geohot

--- src/random/floating_point.c
+++ src/random/floating_point.c
@@ -11,11 +11,32 @@
 #define E(x) ((x>>23) & ((1<<8) - 1)) // Next 8 bits is ecponent 
 #define M(x) (x & ((1<<23) - 1)) // Next 23 bits is mantissa 
 
-void add(int fa, int fb)
+int add(int fa, int fb)
 {
-	if (E(fa) <= E(fb))
-	{
-	}
+	int greater_exp = (E(fa) > E(fb)) ? E(fa) : E(fb);
+
+	int new_fa_mantissa = ((1<<23)|M(fa))>>(greater_exp - E(fa));
+	int new_fb_mantissa = ((1<<23)|M(fb))>>(greater_exp - E(fb));
+
+	//printf("S(fa) = %d\n", S(fa) ? -1 : 1);
+
+	// account for sign before adding the mantissa's 
+	new_fa_mantissa *= S(fa) ? -1 : 1;
+	new_fb_mantissa *= S(fb) ? -1 : 1;
+	
+	int result_mantissa = new_fa_mantissa + new_fb_mantissa;
+
+	// record the sign and remove it from resultant mantissa 
+	int new_sign = result_mantissa<0;
+	result_mantissa *= new_sign ? -1 : 1;
+
+	int shift = result_mantissa>>23;
+	printf("shift = %d\n", shift);
+
+	if (shift == 0) { result_mantissa<<=1; greater_exp -= 1; }
+	else if (shift == 3) { result_mantissa>>=1; greater_exp +=1; }
+	
+	return (new_sign<<31) | ((greater_exp&((1<<8) - 1))<<23) | (result_mantissa&((1<<23) - 1));
 }
 
 int multiply(int fa, int fb) 
@@ -43,23 +64,31 @@ void main()
 	float a = 1.3;
 	float b = 4.5;
 	float c = a*b;
+	float d = a+b;
 
 	int fa = *(uint32_t*)(&a);
 	int fb = *(uint32_t*)(&b); 
 	int fc = *(uint32_t*)(&c);
+	int fd = *(uint32_t*)(&d); 
 
 	printf("\n");
 	printf("%f * %f = %f\n", a, b, c);
 	printf("%x * %x = %x\n", fa, fb, fc);
 	printf("\n");
+	printf("%f + %f = %f\n", a, b, d);
+	printf("%x + %x = %x\n", fa, fb, fd);
+	printf("\n");
 
 	float_point_rep(fa);
 	float_point_rep(fb);
 	float_point_rep(fc);
+	float_point_rep(fd);
 
-	int result = multiply(fa, fb);
+	//int result = multiply(fa, fb);
+	int result = add(fa, fb);
+
 	float_point_rep(result);
 
+	printf("%f + %f = %f\n", a, b, *((float*)&result));
 	printf("\n");
-	printf("%f * %f = %x %f\n", a, b, result, *((float*)&result));
 }
